69fd3c7f9c04e0c7607257b763230ba64929ef13
Felipe Santos <s3t.felipe@gmail.com>
2026-05-29 16:23:43 -0300

    Concatenando
    Fazer variáveis, dar valor a elas e concatenar todas ela pelo resultado da união de todas

--- Aprendendo_PHP.php
+++ Aprendendo_PHP.php
@@ -1,4 +1,11 @@
 <?php
 echo "Olá Mundo";//Inprime na tela e até mesmo variaveis 
 print "Olá Mundo";// Imprime na tela 
+
+$dia=29;
+$mes="maio";
+$ano=2026;
+
+$anocompleto= $dia . "de" . $mes . "de" . $ano;
+
 ?>
