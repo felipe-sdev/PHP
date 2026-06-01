674ea7d574cc7389a3ad533d235b292daacdcbd7
Felipe Santos <s3t.felipe@gmail.com>
2026-06-01 19:59:31 -0300

    Aprendendo Array
    aprendendo a declarar o array e como encontrar o indice

---
+++ Aprendendo sobre array.php
@@ -0,0 +1,11 @@
+<?php
+ $teste_de_array = array("Carro", "barco" , "bicicleta", "ônibus"); //Maneira simples de declarar o array
+
+ $outro_array[]= "Caderno"; 
+ $outro_array[]= "Lapís";
+ $outro_array[]= " borracha";
+ $outro_array[]= "apontador"; // colocando [] a variavel tambem se torna um array
+
+ //Para que saibamos o indicice que se encontra aquele determinado dados no array , devemos simplesmente solicita um print com o nome da variavel de array e ele nos mostrara o numero de cada dados escrito no array.
+
+?>
